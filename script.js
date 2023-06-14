6160a62f4cf1c44a273cbb953c51e732b8742f11
DanielMaximos7 <dmaximos@uwo.ca>
2023-06-14 17:47:47 -0400

    v2

--- script.js
+++ script.js
@@ -1,40 +1,21 @@
-// Generate random data for the graph
-function generateData() {
-    var data = [];
-    for (var i = 0; i < 10; i++) {
-        var randomValue = Math.floor(Math.random() * 100);
-        data.push(randomValue);
+document.addEventListener("DOMContentLoaded", function() {
+    var tabs = document.getElementsByClassName("tab");
+    var contents = document.getElementsByClassName("content");
+
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].addEventListener("click", function(event) {
+            event.preventDefault();
+            var target = this.getAttribute("href").substr(1);
+            showContent(target);
+        });
     }
-    return data;
-}
 
-// Draw the graph using the generated data
-function drawGraph() {
-    var graphElement = document.getElementById('graph');
-    var data = generateData();
-
-    // Use Chart.js to draw the graph
-    new Chart(graphElement, {
-        type: 'bar',
-        data: {
-            labels: ['Label 1', 'Label 2', 'Label 3', 'Label 4', 'Label 5', 'Label 6', 'Label 7', 'Label 8', 'Label 9', 'Label 10'],
-            datasets: [{
-                label: 'Random Data',
-                data: data,
-                backgroundColor: 'rgba(54, 162, 235, 0.5)',
-                borderColor: 'rgba(54, 162, 235, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
+    function showContent(contentId) {
+        for (var i = 0; i < contents.length; i++) {
+            contents[i].classList.remove("show");
         }
-    });
-}
 
-// Call the function to draw the graph when the page loads
-window.onload = drawGraph;
+        var selectedContent = document.getElementById(contentId);
+        selectedContent.classList.add("show");
+    }
+});
