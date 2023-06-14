7bee7fdbc891e57c5ec5a006b76b3fdfcf283972
DanielMaximos7 <dmaximos@uwo.ca>
2023-06-14 17:55:39 -0400

    option spacing

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var tabs = document.getElementsByClassName("tab");
-    var contents = document.getElementsByClassName("content");
-
-    for (var i = 0; i < tabs.length; i++) {
-        tabs[i].addEventListener("click", function(event) {
-            event.preventDefault();
-            var target = this.getAttribute("href").substr(1);
-            showContent(target);
+document.addEventListener('DOMContentLoaded', function() {
+    const tabs = document.querySelectorAll('.tab');
+    const contents = document.querySelectorAll('.content');
+  
+    tabs.forEach(function(tab) {
+      tab.addEventListener('click', function() {
+        const target = this.getAttribute('href').replace('#', '');
+        
+        // Remove active class from all tabs and contents
+        tabs.forEach(function(tab) {
+          tab.classList.remove('active');
         });
-    }
+        contents.forEach(function(content) {
+          content.classList.remove('show');
+        });
+  
+        // Add active class to the clicked tab and show the corresponding content
+        this.classList.add('active');
+        document.getElementById(target).classList.add('show');
+      });
+    });
+  });
+  
 
-    function showContent(contentId) {
-        for (var i = 0; i < contents.length; i++) {
-            contents[i].classList.remove("show");
-        }
-
-        var selectedContent = document.getElementById(contentId);
-        selectedContent.classList.add("show");
-    }
-});
