9fb5408f3e86d839a8961a5c1c7dd39d312c8ada
DanielMaximos7 <dmaximos@uwo.ca>
2023-06-14 18:52:43 -0400

    Chart

--- script.js
+++ script.js
@@ -21,80 +21,55 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
   
-
-// Function to parse the CSV data
-function parseCSV(csv) {
-    const lines = csv.split('\n');
-    const headers = lines[0].split(',');
-
-    const data = lines.slice(1).map(line => {
-        const values = line.split(',');
-        const entry = {};
-
-        headers.forEach((header, index) => {
-            entry[header] = parseFloat(values[index]);
-        });
-
-        return entry;
+  document.addEventListener("DOMContentLoaded", function () {
+    // Load data from CSV
+    Plotly.d3.csv("stock_data.csv", function (err, rows) {
+      if (err) throw err;
+  
+      // Extract the OHLC data
+      var dates = [];
+      var opens = [];
+      var highs = [];
+      var lows = [];
+      var closes = [];
+  
+      rows.forEach(function (row) {
+        dates.push(row.Date);
+        opens.push(parseFloat(row.Open));
+        highs.push(parseFloat(row.High));
+        lows.push(parseFloat(row.Low));
+        closes.push(parseFloat(row.Close));
+      });
+  
+      // Create the candlestick chart
+      var trace = {
+        x: dates,
+        close: closes,
+        decreasing: { line: { color: "red" } },
+        high: highs,
+        increasing: { line: { color: "green" } },
+        line: { color: "rgba(31,119,180,1)" },
+        low: lows,
+        open: opens,
+        type: "candlestick",
+        xaxis: "x",
+        yaxis: "y"
+      };
+  
+      var layout = {
+        dragmode: "zoom",
+        showlegend: false,
+        xaxis: { rangeslider: { visible: false } },
+        yaxis: { autorange: true },
+        margin: { t: 0 }
+      };
+  
+      var data = [trace];
+  
+      Plotly.newPlot("chart", data, layout);
     });
-
-    return data;
-}
-
-// Function to fetch the CSV file
-function fetchCSV(url) {
-    return fetch(url)
-        .then(response => response.text())
-        .then(csv => parseCSV(csv));
-}
-
-// URL of your CSV file
-const csvURL = "stock_data.csv";
-
-// Fetch the CSV file and generate the chart
-fetchCSV(csvURL)
-    .then(data => {
-        // Extract OHLC data from the parsed data
-        const dates = data.map(entry => entry.Date);
-        const open = data.map(entry => entry.Open);
-        const high = data.map(entry => entry.High);
-        const low = data.map(entry => entry.Low);
-        const close = data.map(entry => entry.Close);
-
-        // Create a candlestick trace
-        const trace = {
-            x: dates,
-            close: close,
-            decreasing: { line: { color: 'red' } },
-            high: high,
-            increasing: { line: { color: 'green' } },
-            line: { color: 'rgba(31,119,180,1)' },
-            low: low,
-            open: open,
-            type: 'candlestick',
-            xaxis: 'x',
-            yaxis: 'y'
-        };
-
-        // Set the layout configuration
-        const layout = {
-            title: 'Stock Candlestick Chart',
-            xaxis: {
-                rangeslider: {
-                    visible: false
-                }
-            },
-            yaxis: {
-                title: 'Price'
-            }
-        };
-
-        // Create the chart
-        Plotly.newPlot('chart', [trace], layout);
-    })
-    .catch(error => {
-        console.error('Error loading CSV:', error);
-    });
+  });
+  
 
 
 
