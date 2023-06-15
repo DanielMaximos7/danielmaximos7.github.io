a81f6aded03a2263ef3c82e84d47511e627e55b6
DanielMaximos7 <dmaximos@uwo.ca>
2023-06-15 12:34:32 -0400

    Slide Show

--- script.js
+++ script.js
@@ -20,57 +20,6 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     });
   });
-  
-  document.addEventListener("DOMContentLoaded", function () {
-    // Load data from CSV
-    Plotly.d3.csv("stock_data.csv", function (err, rows) {
-      if (err) throw err;
-  
-      // Extract the OHLC data
-      var dates = [];
-      var opens = [];
-      var highs = [];
-      var lows = [];
-      var closes = [];
-  
-      rows.forEach(function (row) {
-        dates.push(row.Date);
-        opens.push(parseFloat(row.Open));
-        highs.push(parseFloat(row.High));
-        lows.push(parseFloat(row.Low));
-        closes.push(parseFloat(row.Close));
-      });
-  
-      // Create the candlestick chart
-      var trace = {
-        x: dates,
-        close: closes,
-        decreasing: { line: { color: "red" } },
-        high: highs,
-        increasing: { line: { color: "green" } },
-        line: { color: "rgba(31,119,180,1)" },
-        low: lows,
-        open: opens,
-        type: "candlestick",
-        xaxis: "x",
-        yaxis: "y"
-      };
-  
-      var layout = {
-        dragmode: "zoom",
-        showlegend: false,
-        xaxis: { rangeslider: { visible: false } },
-        yaxis: { autorange: true },
-        margin: { t: 0 }
-      };
-  
-      var data = [trace];
-  
-      Plotly.newPlot("chart", data, layout);
-    });
-  });
-  
-
 
 
  // WULHMP2XGLK74SFL
